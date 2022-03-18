69617fe6f8018a5347df0924afc37df1aaf861e7
shubhamshinde-a2d <shubham.21810417@viit.ac.in>
2022-03-19 00:03:26 +0530

    Shop content adjust

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -6,9 +6,10 @@ const Shop = () => {
       <div className="container">
         <div className="shop__text">
           <div>
-            <h1>We know how burgers should be</h1>
-            <h2>The bigger the burger the better the burger</h2>
-            
+            <h1>We know how burgers</h1>
+            <h1>should be, </h1>
+            <h2>The bigger the burger</h2>
+            <h2>better the burger</h2>
           </div>
         </div>
       </div>
